30921fcb120716eecc3952eb92b2b7b2837e9013
Arash Paknezad <a.paknezad@hotmail.com>
2023-08-13 20:37:17 +0200

    feat: export utils types

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import { GridLayout } from './components/GridLayout'
 import { DroppableItem } from './components/DroppableItem'
 import { FlexibleItem } from './components/FlexibleItem'
+import type * as Utils from './helpers/utils'
+import type * as CalculateUtils from './helpers/calculateUtils'
 
-export { GridLayout, DroppableItem, FlexibleItem }
+export { GridLayout, DroppableItem, FlexibleItem, Utils, CalculateUtils }
