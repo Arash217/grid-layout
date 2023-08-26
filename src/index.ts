8c181dc294690711fe2e08fe09f86ad16856aa37
Arash Paknezad <a.paknezad@hotmail.com>
2023-08-26 10:06:56 +0200

    feat: export scalable item

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,15 @@
 import { GridLayout } from './components/GridLayout'
 import { DroppableItem } from './components/DroppableItem'
 import { FlexibleItem } from './components/FlexibleItem'
+import { ScalableItem } from './components/ScalableItem'
 import * as utils from './helpers/utils'
 import * as calculateUtils from './helpers/calculateUtils'
 
-export { GridLayout, DroppableItem, FlexibleItem, utils, calculateUtils }
+export {
+  GridLayout,
+  DroppableItem,
+  FlexibleItem,
+  ScalableItem,
+  utils,
+  calculateUtils,
+}
